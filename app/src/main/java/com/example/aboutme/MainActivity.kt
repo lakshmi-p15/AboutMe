c7270ee7a7480931b54045fe8ca1cb88ae6e4551
lakshmi-p15 <lakshmi.pavuluri15@gmail.com>
2022-09-05 23:53:57 -0700

    updated with DataBinding

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,24 +1,23 @@
 package com.example.aboutme
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private val myName = MyName(name = "Sailakshmi")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.nameText.text = myName.name
         binding.doneButton.setOnClickListener {
             addNickname(it)
         }
@@ -27,10 +26,13 @@ class MainActivity : AppCompatActivity() {
     fun addNickname(view: View) {
 
         binding.apply {
-            binding.nicknameText.text = binding.nicknameEdit.text
-            binding.nicknameEdit.visibility = View.GONE
+            // nicknameText.text = nicknameEdit.text
+            myName?.nickName = nicknameEdit.text.toString()
+            // Invalidate all binding expressions and request a new rebind to refresh UI
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
             view.visibility = View.GONE
-            binding.nicknameText.visibility = View.VISIBLE
+            nicknameText.visibility = View.VISIBLE
         }
 
         // Hide the keyboard.
